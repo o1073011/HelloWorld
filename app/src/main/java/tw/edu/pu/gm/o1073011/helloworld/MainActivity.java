bf674ad3198ea253d7a81a920a5529a1f95849a1
楊忠誠 <o1073011@gm.pu.edu.tw>
2020-05-13 13:52:59 +0800

    加入master分支程式碼

--- app/src/main/java/tw/edu/pu/gm/o1073011/helloworld/MainActivity.java
+++ app/src/main/java/tw/edu/pu/gm/o1073011/helloworld/MainActivity.java
@@ -13,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView txv = (TextView) findViewById(R.id.txv);
         txv.setText("Hello tcyang");
+
+        TestMaster();
     }
+
+    public void TestMaster(){
+        TextView txv = (TextView) findViewById(R.id.txv);
+        txv.setText("Master分支");
+    }
+
 }
